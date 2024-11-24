8c10b17430244076bcb3c32309c226ad602834a6
ye ziying <yziying8@gmail.com>
2024-11-24 22:43:13 +0000

    transfer function from data_mining.py to data_analysis.py

---
+++ code/data_analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import json as jn
+
+def map_data (df_to_map):
+    '''This function maps the numeric data in the csv file and gives the context provided in the JSON file'''
+    # Load the structure from json file to map 
+    with open ('data/data_dictionary.json', 'r') as f:
+        map = jn.loads(f)
+    
+    # apply the logic to map the context to each encoded number
+    for col, col_map in map.items():
+        # check if the column exist in the csv data's colum
+        if col in df_to_map.columns:
+            df_to_map[col] = df_to_map[col].map(col_map).fillna('Unknown')
+
+    return df_to_map
--- code/data_mining.py
+++ code/data_mining.py
@@ -2,33 +2,24 @@ import pandas as pd
 import json as jn
 
 
-def map_data ():
-    '''This function maps the numeric data in the csv file and gives the context provided in the JSON file'''
-    # Read the csv file in the data folder
-    csv_data = pd.read_csv('data/Scotland_teaching_file_1PCT.csv', 'r')
-    csv_data = csv_data.drop_duplicates()
 
-    # load the data from the data_dictionary.json file
-    with open ('data/data_dictionary.json', 'r') as f:
-        map = jn.load(f)
+def clean_data ():
+    '''This function checks for consistency and clean the data'''
+    data = pd.read_csv('data/Scotland_teaching_file_1PCT.csv')
+    # This drops the duplication and fill the missing values with 'Unknown'
+    data = data.drop_duplicates().fillna('Unknown')
+
+
+
+
+# def build_input_states():
+#     for colunm_type in data_json:
+#         #store keys in list
     
-    # apply the logic to map the context to each encoded number
-    for col, col_map in map.items():
-        # check if the column exist in the csv data's colum
-        if col in csv_data.columns:
-            csv_data[col] = csv_data[col].map(col_map).fillna('Unknown')
-
-    # save the mapped data in another csv file
-    csv_data.to_csv('cleaned_data.csv', index = False)
-
-    return csv_data
-
-    
-
-
-
-    
-
-
+#     for row in data:
+#         for data_point in row:
+#             if data_point not in list
+#                 get rid of row 
+        # "sasdasd".isnumeric
 
 
