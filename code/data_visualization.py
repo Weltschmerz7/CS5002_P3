c67c77a3e81f215f904e510aaba629ff81474ac3
ye ziying <yziying8@gmail.com>
2024-11-27 18:04:38 +0000

    add data_visualization.py module

---
+++ code/data_visualization.py
@@ -0,0 +1,8 @@
+import matplotlib as ml
+
+def get_bar (data_to_plot):
+    '''This function takes in the data to plot, and make the bar chart '''
+
+def get_pie (data_to_plot):
+    '''This function takes in the data to plot, and make the pie chart'''
+
