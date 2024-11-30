3199260c2d0c00b35e7314149373c1d612fc011b
ye ziying <yziying8@gmail.com>
2024-11-30 13:04:06 +0000

    update data_analysis.py to accomodate for mapped data

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -34,13 +34,23 @@ def analyze_csv (csv_data):
     # Requirement 9: get the number of records by occupation and approximate social grade
     results['occupation_by_social_grade'] = (data.groupby(['occupation', 'approximate_social_grade']).size().to_dict())
     # Requirement 10: get the number of economically active people depending on age
-    active_codes = ['1', '2', '3', '4']  # economically active codes
+    active_codes = [
+        'Economically active: Employed',
+        'Economically active: Self-Employed', 
+        'Economically active: Unemployed', 
+        'Economically active: Full-time student'
+        ]  # economically active codes
     results['economically_active_by_age'] = (data.loc[data['economic_activity'].isin(active_codes)].groupby('age').size().to_dict())
     # Requirement 11: get the number of economically inactive people depending on a health descriptor
-    inactive_codes = ['5', '6', '7', '8', '9']  # economically inactive codes
+    inactive_codes = ['Economically inactive: Retired', 
+                      'Economically inactive: Student', 
+                      'Economically inactive: Looking after home or family', 
+                      'Economically inactive: Long-term sick or disabled', 
+                      'Economically inactive: Other'
+                      ]  # economically inactive codes
     results['economically_inactive_by_health'] = (data.loc[data['economic_activity'].isin(inactive_codes)].groupby('health').size().to_dict())
     # Requirement 12: get the number of working hours per week for students
-    student_codes = ['4', '6']  # full-time and part-time student codes
+    student_codes = ['Economically active: Full-time student', 'Economically inactive: Student']  # full-time and part-time student codes
     results['working_hours_for_students'] = (data.loc[data['economic_activity'].isin(student_codes)].groupby('hours_worked_per_week').size().to_dict())
     # print(f'one of the result dictionary! : {results["age_count"]}')
     print(results)
