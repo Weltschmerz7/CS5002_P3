05c293300a8d5af6d07ac41b7017336371b43dab
ye ziying <yziying8@gmail.com>
2024-11-30 22:32:04 +0000

    add dict_to_df function to transform dictionaries to df

--- code/data_visualization.py
+++ code/data_visualization.py
@@ -1,9 +1,26 @@
-import matplotlib as ml
-from data_analysis import analyze_csv
-def get_bar (data_to_plot):
-    '''This function takes in the data to plot, and make the bar chart '''
+import pandas as pd
 
 
-def get_pie (data_to_plot):
-    '''This function takes in the data to plot, and make the pie chart'''
+
+def dict_to_df (dict, col_names = None):
+    '''Tihs helper function turns dictionary to df, for readability and consistency'''
+    if isinstance(next(iter(dict.keys())), tuple):
+        # handle the transformation if the keys are tuple
+        df = pd.DataFrame.from_dict(dict, orient='index', columns=[col_names[-1]])
+        df.reset_index(inplace=True)
+        df[col_names[:-1]] = pd.DataFrame(df['index'].tolist(), index=df.index)
+        df.drop(columns='index', inplace=True)
+        return df[col_names]
+    else:
+        # if the dictionary just have simple key value pair, transform them
+        df = pd.DataFrame.from_dict(dict, orient='index', columns=[col_names[1]])
+        df.reset_index(inplace=True)
+        df.columns = col_names
+        return df
+# def get_bar (data_to_plot):
+#     '''This function takes in the data to plot, and make the bar chart '''
+
+
+# def get_pie (data_to_plot):
+#     '''This function takes in the data to plot, and make the pie chart'''
 
