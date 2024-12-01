a94e090c82e7e949ac5e0d80d307305405909c97
ye ziying <yziying8@gmail.com>
2024-12-01 11:49:41 +0000

    update ipynb_to_html.py

--- code/ipynb_to_html.py
+++ code/ipynb_to_html.py
@@ -1,3 +1,14 @@
 import os
 import subprocess
 
+def convert_to_html (notebook):
+    '''This function can turn the jupyter notebook into high quality html format'''
+    # run nbconvert to convert the notebook to HTML
+    subprocess.run(['jupyter','nbconvert', '--to', 'html', notebook], check=True)
+    html_report = notebook.replace('.ipynb', '.html')
+    print(f'html version of the notebook have been created {html_report}')
+
+
+
+# generate high quality report
+# jupyter nbconvert --to html --template classic <notebook.ipynb>
